04a8f69c2af96860af36f5ddbf95ac7178f420a8
YagoGV <yagogv@gmail.com>
2025-01-11 11:28:09 +0100

    aboutme listo

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
-  imports: [FontAwesomeModule, CommonModule],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
