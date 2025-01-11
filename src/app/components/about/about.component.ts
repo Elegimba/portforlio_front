1811cebdf0ed951faa26c769fdbd318e956ae84a
YagoGV <yagogv@gmail.com>
2025-01-11 11:25:27 +0100

    traspaso del exceso de header y about listo

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-about',
+  imports: [FontAwesomeModule, CommonModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+  show: boolean = false;
+  faChevronDown = faChevronDown;
+
+  showContent(): void {
+    this.show = !this.show;
+  }
+}
