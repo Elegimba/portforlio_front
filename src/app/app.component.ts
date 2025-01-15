24b3b473cc5a66496e1804fbf1fe8969b1855008
YagoGV <yagogv@gmail.com>
2025-01-15 09:36:20 +0100

    footer listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,19 @@ import { HeaderComponent } from "./components/header/header.component";
 import { AboutComponent } from "./components/about/about.component";
 import { StoryComponent } from "./components/story/story.component";
 import { ProyectsComponent } from "./components/proyects/proyects.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, StoryComponent, ProyectsComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, StoryComponent, ProyectsComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio_front';
+
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
   
 }
